e7e46b1d1a7e4885abb2ff46423e1968cd7672d3
Hangqi Wu <wuhangqi25@gmail.com>
2018-10-12 09:57:18 -0700

    Add a exit function without parameter
    In order to support user space programs that use old version of exit
    function, I created function `void exit(void)`. Now `exit()` is
    equivalent to `exit(0)`.

--- defs.h
+++ defs.h
@@ -105,6 +105,7 @@ int             pipewrite(struct pipe*, char*, int);
 // proc.c
 int             cpuid(void);
 void            exit(int);
+void            exit(void);
 int             fork(void);
 int             growproc(int);
 int             kill(int);
