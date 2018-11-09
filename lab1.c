1e1a9a04df4bc211e371ae0c7e51cbfbb1b0382c
Hangqi Wu <wuhangqi25@gmail.com>
2018-11-09 10:07:55 -0800

    Add priority aging test

--- lab1.c
+++ lab1.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 	int exitWait(void);
 	int waitPid(void);
     int PScheduler(void);
-
+    int PriorityAgingTest(void);
   printf(1, "\n This program tests the correctness of your lab#1\n");
   
   if (atoi(argv[1]) == 1)
@@ -16,7 +16,9 @@ int main(int argc, char *argv[])
 	waitPid();
   else if (atoi(argv[1]) == 3)
     PScheduler();
-   else
+  else if (atoi(argv[1]) == 4)
+    PriorityAgingTest();
+  else
     printf(1, "\ntype \"lab1 1\" to test exit and wait, \"lab1 2\" to test waitpid and \"lab1 3\" to test the priority scheduler \n");
   
     // End of test
@@ -136,6 +138,47 @@ int PScheduler(void){
       //printf(1,"\n This is the parent: child with PID# %d has finished with status %d \n",ret_pid,exit_status);
     }
     printf(1,"\n if processes with highest priority finished first then its correct \n");
+  }
+
+  if (ret_pid == 0) {} // add this line to avoid ret_pid not used warning
+
+  return 0;
+}
+
+int PriorityAgingTest(void) {
+  int pid, ret_pid, exit_status;
+  int i,j,k;
+  int place = 0;
+
+  setpriority(0);
+  for (i = 0; i <  3; i++) {
+    pid = fork();
+    if (pid > 0 ) {
+      continue;}
+    else if ( pid == 0) {
+      setpriority(20+i);
+      printf(1, "\n Hello! this is child# %d and I have priority of  %d \n",getpid(),20+i);
+      for (j=0;j<50000;j++) {
+        for(k=0;k<10000;k++) {
+          asm("nop"); }}
+      int priority = getpriority(getpid());
+      printf(1, "\n child# %d has finished! My priority is %d  \n", getpid(),priority);
+      exit(0);
+    }
+    else {
+      printf(2," \n Error \n");
+      exit(-1);
+    }
+  }
+
+
+
+  if(pid > 0) {
+    for (i = 0; i <  3; i++) {
+      ret_pid = wait(&exit_status);
+      //printf(1,"\n This is the parent: child with PID# %d has finished with status %d \n",ret_pid,exit_status);
+    }
+    printf(1,"\n   \n");
   }
 
   if (ret_pid == 0) {} // add this line to avoid ret_pid not used warning
