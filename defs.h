d9c02d14a27e29b8ceeaa97c7520608cefde654e
Hangqi Wu <wuhangqi25@gmail.com>
2018-11-09 10:18:27 -0800

    Fix: Implicit declaration of function getpriority

--- defs.h
+++ defs.h
@@ -120,6 +120,7 @@ void            userinit(void);
 int             wait(int*);
 int             waitpid(int, int*, int);
 int             setpriority(int);
+int             getpriority(int);
 void            wakeup(void*);
 void            yield(void);
 
