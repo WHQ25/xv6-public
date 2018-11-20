33efac2321ac34ff8848dbbfe9c39bb18aff16bf
jvillan123 <32605272+jvillan123@users.noreply.github.com>
2018-11-20 14:52:45 -0800

    Added todo

--- trap.c
+++ trap.c
@@ -7,7 +7,9 @@
 #include "x86.h"
 #include "traps.h"
 #include "spinlock.h"
-
+//=================================================================================
+//=====================TODO 5: Add trap case for page fault========================
+//=================================================================================
 // Interrupt descriptor table (shared by all CPUs).
 struct gatedesc idt[256];
 extern uint vectors[];  // in vectors.S: array of 256 entry pointers
