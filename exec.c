1332d22b619ba983ad2be2e03104f6735eef0c4c
jvillan123 <32605272+jvillan123@users.noreply.github.com>
2018-11-20 14:34:41 -0800

    Added TODO

--- exec.c
+++ exec.c
@@ -62,6 +62,9 @@ exec(char *path, char **argv)
 
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
+  //====================================================================================================================
+  //==================================TODO 1:Change code block to move the stack so it can grow=========================
+  //====================================================================================================================
   sz = PGROUNDUP(sz);
   if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
     goto bad;
