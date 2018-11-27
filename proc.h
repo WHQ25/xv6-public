afa19ad7a04c36fb0cfa22bd900c299f7486324f
jvillan123 <32605272+jvillan123@users.noreply.github.com>
2018-11-26 23:12:39 -0800

    added pages variable

--- proc.h
+++ proc.h
@@ -51,6 +51,7 @@ struct proc {
   char name[16];               // Process name (debugging)
   int exitStatus;              // Process exit status
   int priority;                // Priority value for scheduling
+  int pages;                   // Memory page number
 };
 
 // Process memory is laid out contiguously, low addresses first:
