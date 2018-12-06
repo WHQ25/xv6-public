4819d7749dd5792ba7dc26e2f68d93111a86fc91
Hangqi Wu <wuhangqi25@gmail.com>
2018-12-06 01:09:42 -0800

    Change position of stack
    Implement TODO 1 and TODO 2
    * Introduce a new process property `spc` to counter stack pages
    * Change stack position to the top of user memory space

--- exec.c
+++ exec.c
@@ -12,7 +12,7 @@ exec(char *path, char **argv)
 {
   char *s, *last;
   int i, off;
-  uint argc, sz, sp, ustack[3+MAXARG+1];
+  uint argc, sz, spc, sp, ustack[3+MAXARG+1];
   struct elfhdr elf;
   struct inode *ip;
   struct proghdr ph;
@@ -60,13 +60,14 @@ exec(char *path, char **argv)
   end_op();
   ip = 0;
 
-  // Allocate two pages at the next page boundary.
-  // Make the first inaccessible.  Use the second as the user stack.
-  sz = PGROUNDUP(sz);
-  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
+  // Allocate one page of memory at the top of the user memory space
+  // Set stack page counter to 1
+  // Set stack pointer to the top word of the page
+  if(allocuvm(pgdir, KERNBASE - PGSIZE, KERNBASE - PGSIZE + 1) == 0)
     goto bad;
-  clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
-  sp = sz;
+  spc = 1;
+  sp = KERNBASE - 1;
+
 
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
@@ -97,6 +98,7 @@ exec(char *path, char **argv)
   oldpgdir = curproc->pgdir;
   curproc->pgdir = pgdir;
   curproc->sz = sz;
+  curproc->spc = spc;
   curproc->tf->eip = elf.entry;  // main
   curproc->tf->esp = sp;
   switchuvm(curproc);
