015ffe4b85cb2cb4e9c2a65eda60e229eae907ac
Hangqi Wu <wuhangqi25@gmail.com>
2018-12-06 02:28:08 -0800

    Update copyuvm()
    * Implement TODO 4
    * copyuvm() now has 3 parameters, spc is stack page counter, add a loop
    to copy user stack pages

--- vm.c
+++ vm.c
@@ -313,7 +313,7 @@ clearpteu(pde_t *pgdir, char *uva)
 // Given a parent process's page table, create a copy
 // of it for a child.
 pde_t*
-copyuvm(pde_t *pgdir, uint sz)
+copyuvm(pde_t *pgdir, uint sz, uint spc)
 {
   pde_t *d;
   pte_t *pte;
@@ -337,6 +337,25 @@ copyuvm(pde_t *pgdir, uint sz)
       goto bad;
     }
   }
+
+  // add another loop to copy user stack
+  for(i = 0; i < spc; i++) {
+    if((pte = walkpgdir(pgdir, (void *)(KERNBASE - (i + 1)*PGSIZE), 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)(KERNBASE - (i + 1)*PGSIZE),
+                PGSIZE, V2P(mem), flags) < 0) {
+      kfree(mem);
+      goto bad;
+    }
+  }
+
   return d;
 
 bad:
